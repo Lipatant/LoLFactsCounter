2e98652c645889fe9d08c824e51edf915392f256
Lipatant <virgile.berrier@gmail.com>
2025-02-16 12:20:43 +0100

    feat: adding champion selection

--- src/index.js
+++ src/index.js
@@ -5,23 +5,67 @@ const champions = [
 const championGridAlly = document.getElementById("champion-grid-ally");
 const championGridEnemy = document.getElementById("champion-grid-enemy");
 
+function disableChampionGridElement(championGridElement) {
+    let button = championGridElement.children[0];
+    button.disabled = false;
+}
+
+function enableChampionGridElement(championGridElement) {
+    let button = championGridElement.children[0];
+    button.disabled = true;
+}
+
 function getChampionName(id) {
     return id.charAt(0).toUpperCase() + id.slice(1);
 }
 
 function makeChampionGrid(element) {
     champions.forEach(
-        function (id, index) {
+        function(id, index) {
             element.appendChild(makeChampionGridElement(id));
         }
     );
+    element.setAttribute("champion-id", "")
 }
 
 function makeChampionGridElement(id) {
     let element = document.createElement("li");
-    element.innerHTML =  `<img src="img/champions/${id}.webp" alt="${getChampionName(id)}"></img>`;
+    let elementButton = document.createElement("button");
+    elementButton.addEventListener("click", onChampionGridElementClick);
+    elementButton.innerHTML = `<img alt="${getChampionName(id)}" src="img/champions/${id}.webp"></img>`;
+    element.appendChild(elementButton);
+    element.setAttribute("champion-id", id);
     return element;
 }
 
-makeChampionGrid(championGridAlly);
-makeChampionGrid(championGridEnemy);
+function onChampionGridElementClick(event) {
+    let eventTarget = (event.target.nodeName == "BUTTON") ? event.target : event.target.parentNode;
+    let championGridElement = eventTarget.parentNode;
+    selectChampionGridElement(championGridElement);
+}
+
+function selectChampionGridChampion(championGrid, id) {
+    let championGridChildren = championGrid.children;
+    for (let i = 0; i < championGridChildren.length; i++) {
+        let championGridElement = championGridChildren[i];
+        if (championGridElement.getAttribute("champion-id") === id) {
+            enableChampionGridElement(championGridElement);
+        } else {
+            disableChampionGridElement(championGridElement);
+        }
+    }
+    championGrid.setAttribute("champion-id", id);
+}
+
+function selectChampionGridElement(championGridElement) {
+    let championGrid = championGridElement.parentNode;
+    selectChampionGridChampion(championGrid, championGridElement.getAttribute("champion-id"))
+}
+
+const championGridAll = [championGridAlly, championGridEnemy];
+
+championGridAll.forEach(
+    function(championGrid, index) {
+        makeChampionGrid(championGrid);
+    }
+);
